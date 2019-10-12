4131ed9f4defa23c8c115f00d728ec284f07459c
Armando Acosta Guerrero <darman.89@gmail.com>
2019-10-12 15:57:03 -0500

    Cantidad de elementos de la lista de portafolios (VERDE)

--- KataTDD/views.py
+++ KataTDD/views.py
@@ -5,8 +5,10 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.decorators.csrf import csrf_exempt
 
+from KataTDD.models import Portafolio
+
 
 @csrf_exempt
 def index(request):
-    portafolio_list = []
+    portafolio_list = Portafolio.objects.all()
     return HttpResponse(serializers.serialize("json", portafolio_list))
