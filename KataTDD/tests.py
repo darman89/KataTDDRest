5d44534855d44b89fefc430b79932bfa5e681a5f
Armando Acosta Guerrero <darman.89@gmail.com>
2019-10-12 15:47:15 -0500

    Status de respuesta de lista de portafolios (VERDE)

--- KataTDD/tests.py
+++ KataTDD/tests.py
@@ -1,7 +1,6 @@
-from django.test import TestCase, Client
+from django.test import TestCase
 
-# Create your tests here.
-import json
+
 # Create your tests here.
 
 
--- KataTDD/views.py
+++ KataTDD/views.py
@@ -1,3 +1,12 @@
+from django.core import serializers
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.decorators.csrf import csrf_exempt
+
+
+@csrf_exempt
+def index(request):
+    portafolio_list = []
+    return HttpResponse(serializers.serialize("json", portafolio_list))
