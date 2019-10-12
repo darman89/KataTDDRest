1b94b128cb4b243b65c7c8e5e8ba2e97b59e0917
Maria Arevalo <mariaisabelarevalo@hotmail.com>
2019-10-12 15:40:08 -0500

    Status de respuesta de lista de portafolios (ROJO)

--- KataTDD/tests.py
+++ KataTDD/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+import json
+# Create your tests here.
+
+
+class PortafolioTestCase(TestCase):
+
+    def test_list_portafolios(self):
+        url = '/portafolio/'
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, 200)
